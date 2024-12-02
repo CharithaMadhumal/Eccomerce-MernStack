9113ad6f421a4da90173faf46b91eef60061e67a
CharithaMadhumal <charithamadhumal174@gmail.com>
2024-12-03 00:02:10 +0530

    5th commit

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,7 +1,7 @@
 import {Router} from 'express'
 import  auth  from '../middleware/auth.js'
 import upload from '../middleware/multer.js'
-import { loginController, logoutController, registerUserController, uploadAvatar, verifyEmailController } from '../controllers/user.controller.js'
+import { forgotPasswordController, loginController, logoutController, registerUserController, updateUserDetails, uploadAvatar, verifyEmailController } from '../controllers/user.controller.js'
 
 const userRouter = Router()
 
@@ -10,5 +10,7 @@ userRouter.post('/verify-email',verifyEmailController)
 userRouter.post('/login',loginController)
 userRouter.get('/logout',auth,logoutController)
 userRouter.put('/upload-avatar',auth,upload.single('avatar'),uploadAvatar)
+userRouter.put('/update-user',auth,updateUserDetails)
+userRouter.put('/forgot-password',auth,forgotPasswordController)
 
 export default userRouter
